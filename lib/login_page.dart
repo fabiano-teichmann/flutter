7ed84ca9f2a2934870c4b4d47114349e6db4db47
Fabiano Teichmann <fabiano.teichmann@hbsis.com.br>
2020-12-06 19:35:53 -0300

    Widget stack

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -9,57 +9,71 @@ class _LoginPageState extends State<LoginPage> {
   String email = "";
   String password = "";
 
+  Widget _body() {
+    return SingleChildScrollView(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: 80,
+              height: 80,
+              child: Image.asset('assets/images/cadeado.png'),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            TextField(
+              onChanged: (text) {
+                email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                  labelText: 'Email', border: OutlineInputBorder()),
+            ),
+            SizedBox(height: 20),
+            TextField(
+              obscureText: true,
+              onChanged: (text) {
+                password = text;
+              },
+              decoration: InputDecoration(
+                  labelText: 'Senha', border: OutlineInputBorder()),
+            ),
+            SizedBox(height: 20),
+            RaisedButton(
+              onPressed: () {
+                if (email == 'fabiano@gmail.com' && password == 'carai') {
+                  //Navigator.of(context).pushReplacementNamed without return
+                  Navigator.of(context).pushNamed('/home');
+                } else {
+                  print('Errou !!!');
+                }
+              },
+              child: Text('Entrar'),
+              color: Colors.blue[200],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: 80,
-                height: 80,
-                child: Image.asset('assets/images/cadeado.png'),
-              ),
-              SizedBox(
-                height: 30,
-              ),
-              TextField(
-                onChanged: (text) {
-                  email = text;
-                },
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                    labelText: 'Email', border: OutlineInputBorder()),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                obscureText: true,
-                onChanged: (text) {
-                  password = text;
-                },
-                decoration: InputDecoration(
-                    labelText: 'Senha', border: OutlineInputBorder()),
-              ),
-              SizedBox(height: 20),
-              RaisedButton(
-                onPressed: () {
-                  if (email == 'fabiano@gmail.com' && password == 'carai') {
-                    //Navigator.of(context).pushReplacementNamed without return
-                    Navigator.of(context).pushNamed('/home');
-                  } else {
-                    print('Errou !!!');
-                  }
-                },
-                child: Text('Entrar'),
-                color: Colors.blue[200],
-              ),
-            ],
+      body: Stack(
+        children: [
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            // height: MediaQuery.of(context).size.height,
+            child: Image.asset('assets/images/background.jpg', fit: BoxFit.cover,),
           ),
-        ),
+          Container(color: Colors.black.withOpacity(0.3),),
+          _body(),
+        ],
       ),
     );
   }
