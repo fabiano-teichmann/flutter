ebfda8d5a081c825117fdc7da4efc28955692f62
Fabiano Teichmann <fabiano.teichmann@hbsis.com.br>
2020-12-07 18:27:53 -0300

    Menu

--- lib/widget/navigation.dart
+++ lib/widget/navigation.dart
@@ -4,26 +4,34 @@ class Navigation extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Drawer(
-        child: Column(
-          children: [
-            ListTile(
-              leading: Icon(Icons.home),
-              title: Text('Home'),
-              subtitle: Text('Pagina inicial'),
-              onTap: (){
-                Navigator.of(context).pushNamed('/home');
-              },
+      child: Column(
+        children: [
+          UserAccountsDrawerHeader(
+            currentAccountPicture: ClipOval(
+              child: Image.network(
+                  'https://avatars0.githubusercontent.com/u/29966200?s=60&v=4'),
             ),
-            ListTile(
-              leading: Icon(Icons.login),
-              title: Text('Logout'),
-              subtitle: Text('Fazendo logout'),
-              onTap: (){
-                Navigator.of(context).pushNamed('/');
-              },
-            ),
-          ],
-        ),
-      );
+            accountEmail: Text('Fabiano'),
+            accountName: Text('fabiano@gmail.com'),
+          ),
+          ListTile(
+            leading: Icon(Icons.home),
+            title: Text('Home'),
+            subtitle: Text('Pagina inicial'),
+            onTap: () {
+              Navigator.of(context).pushNamed('/home');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.login),
+            title: Text('Logout'),
+            subtitle: Text('Fazendo logout'),
+            onTap: () {
+              Navigator.of(context).pushNamed('/');
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
